8be056cbaa79ba6580bf8c99ddc249c0c2b162d5
Unknown <boratko@gmail.com>
2019-03-20 12:12:07 -0400

    Adjusted DeltaBoxes to be compatible with UnitBoxes api
    Changed calc_dim_scales to only output dimension scales.
    Added forward pass for DeltaBoxes.

--- boxes/delta_boxes.py
+++ boxes/delta_boxes.py
@@ -9,25 +9,36 @@ class DeltaBoxes(Module):
         self.z = Parameter(torch.rand(num_models, num_boxes, dim))
         self.logdelta = Parameter(torch.rand(num_models, num_boxes, dim))
 
+    def forward(self, ids=slice(None, None, None), scaled = False, **kwargs):
+        z = self.min(ids, scaled, **kwargs)
+        Z = self.max(ids, scaled, **kwargs)
+        return torch.stack((z,Z), dim=2)
+
     def min(self, ids=slice(None, None, None), scaled = False, **kwargs):
         z = self.z[:, ids]
         if scaled:
-            min_z, dim_scales = self.calc_dim_scales()
+            min_z = self.calc_min_z()
+            dim_scales = self.calc_dim_scales()
             return (z - min_z) / dim_scales
         else:
             return z
 
-
     def max(self, ids=slice(None, None, None), scaled = False, **kwargs):
         Z = self.z[:, ids] + torch.exp(self.logdelta[:, ids])
         if scaled:
-            min_z, dim_scales = self.calc_dim_scales()
+            min_z = self.calc_min_z()
+            dim_scales = self.calc_dim_scales()
             return (Z - min_z) / dim_scales
         else:
             return Z
 
+    def calc_min_z(self):
+        min_z, _ = torch.min(self.z, dim=1)
+        return min_z
+
     def calc_dim_scales(self):
         max_Z, _ = torch.max(self.z + torch.exp(self.logdelta), dim=1)
-        min_z, _ = torch.min(self.z, dim=1)
+        min_z = self.calc_min_z()
         dim_scales = max_Z - min_z
-        return min_z, dim_scales
+        return dim_scales
+
