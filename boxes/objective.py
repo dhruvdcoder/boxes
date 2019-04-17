eceebfaaddb8c6357601f5d2d58769f73fa785e5
Unknown <boratko@gmail.com>
2019-04-17 16:42:41 -0400

    Updated objective for sacred experiments

--- boxes/objective.py
+++ boxes/objective.py
@@ -4,6 +4,8 @@ from pprint import pprint
 from sqlite3 import Connection
 from dataclasses import dataclass, field
 import math
+from sacred import Experiment
+
 
 @dataclass
 class Objective:
@@ -24,6 +26,7 @@ class Objective:
     hyper_opt_id: Optional[int] = None
     sql_conn: Optional[Connection] = None
     properties: dict = field(default_factory=dict)
+    ex: Experiment = field(default_factory=Experiment)
 
     def __post_init__(self):
         self.num_boxes = self.unary_probs.shape[0]
@@ -31,7 +34,7 @@ class Objective:
         if self.gradient_clipping != (None, None):
             self.extra_callbacks.append(GradientClipping(*self.gradient_clipping))
 
-    def objective(self, **hyperparameters):
+    def objective(self, ex, **hyperparameters):
         # Fix random seed
         random_seed = np.random.randint(2**32 - 1)
         torch.manual_seed(random_seed)
@@ -40,9 +43,8 @@ class Objective:
         print(f"\n[{self.hyper_opt_iteration}] (Hyperoptimization Iteration)")
         pprint(hyperparameters)
 
-        neptune.init(project_qualified_name="mboratko/sandbox")
-        neptune.create_experiment(
-            params={
+        ex.add_config(
+            {
                 **self.properties,
                 **hyperparameters,
                 "torch_random_seed": random_seed,
@@ -88,8 +90,8 @@ class Objective:
             MetricCallback(rec_col.dev, self.dev, metric_spearman_r),
             PercentIncreaseEarlyStopping(rec_col.dev, "mean_cond_kl_loss", 0.25, 10),
             PercentIncreaseEarlyStopping(rec_col.dev, "mean_cond_kl_loss", 0.5),
-            NeptuneCallback(rec_col.dev, "VALID"),
-            NeptuneCallback(rec_col.train, "TRAIN"),
+            SacredCallback(rec_col.dev, ex, "VALID"),
+            SacredCallback(rec_col.train, ex, "TRAIN"),
             StopIfNaN(),
             *self.extra_callbacks,
         )
